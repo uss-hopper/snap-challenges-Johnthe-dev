06d0eb50524b0af33a2efb2610596612e13f22ea
John Johnson-Rodgers <john@johnthe.dev>
2019-10-14 08:31:59 -0600

    finished snap challenge

--- snap-10-14-js-unctions/script.js
+++ snap-10-14-js-unctions/script.js
@@ -1,33 +1,37 @@
 function convertToGrams(weight, unit) {
 	if(unit.toLowerCase()==="lbs") {
-		return weight * 453.592
+		return weight * 453.592;
 	} else if (unit.toLowerCase()==="oz") {
-		return weight * 28.3495
+		return weight * 28.3495;
 	} else if (unit.toLowerCase()==="kg") {
-		return weight * 1000
+		return weight * 1000;
 	} else if (unit.toLowerCase()==="g") {
-		return weight * 1
+		return weight * 1;
 	} else if (unit.toLowerCase()==="mg") {
-		return weight * .001
+		return weight * .001;
 	} else {
 		return "Not a valid unit, try lbs, oz, kg, g, or mg."
 	}
 }
 
 function sumPositiveFactors(number) {
-	 if (number.isInteger() === true) {
+	 if (Number.isInteger(number) === true) {
 		if (number>0) {
 			let counter = 1;
-			let factors = 0;
-			while (counter<number){
-				if (number%counter==0) {
-
+			let factorsAdded = 0;
+			while (counter<=number){
+				if (number%counter === 0) {
+					factorsAdded = factorsAdded + counter;
 				}
+				counter = counter + 1;
 			}
+			return factorsAdded;
 		} else {
-			return "Number needs to be positive."
+			return "Number needs to be positive.";
 		}
 	 } else {
-	 	return "Number is not a positive integer."
+	 	return "Number is not a positive integer.";
 	 }
 }
+
+console.log(sumPositiveFactors(12));
