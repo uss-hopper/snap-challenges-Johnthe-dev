18ea16c62049aa14bb86318c5511e5cbba388b8b
John Johnson-Rodgers <john@johnthe.dev>
2019-12-02 08:33:54 -0700

    commented javascript

--- snap-12-2-map-to-DOM/snap-12-2.js
+++ snap-12-2-map-to-DOM/snap-12-2.js
@@ -1,19 +1,19 @@
 names = ['Bob', 'Jim', 'Matt', 'Mary', 'Suzy', ' Sarah'];
-
+//creates html item with an ID of the input that contains the input as plaintext
 function createListItemHtml(itemName) {
 	return `<div id="${itemName}">
 					${itemName}
 		</div>`;
 }
-
+//inserts each element in array into it's own html element, only use for unique elements
 function insertArrayIntoHtml(array) {
 	array.map(function(person) {
-		let itemHtml = createListItemHtml(person.value);
+		let itemHtml = createListItemHtml(person);
 		let itemListRef = document.getElementById("results");
 		itemListRef.insertAdjacentHTML("afterend", itemHtml);
 	});
 }
-
+//for onclick
 function performAction() {
 	insertArrayIntoHtml(names);
 }
